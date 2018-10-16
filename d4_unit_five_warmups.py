610c7be3d7c1edd26d6f9892361a6710f8856258
Flint <flint@iMac1.local>
2018-10-16 11:30:27 -0400

    infinite loops can be good

---
+++ d4_unit_five_warmups.py
@@ -0,0 +1,6 @@
+while True:
+    play_game = input("Would you like to  play a game? (y/n) ")
+    if play_game == "y" or play_game == "n":
+        break
+    else:
+        print("Please enter a valid answer")
